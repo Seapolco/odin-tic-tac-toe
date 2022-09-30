9b583fc91c4d7061b1f1493ec03cb51cda2937ec
Seapolco <seapolco@gmail.com>
2022-09-30 20:15:43 +0100

    Add dynamic functionality to between gameboard, displayController and playerController.

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ console.log('Hello World!');
 
 //factory function for players
 
+const playerController = (() => {
+    const playerOne = 'X';
+    const playerTwo = 'O';
+    let lastMove = '';
+    let moves = 0;
+
+    return {playerOne, playerTwo, lastMove, moves}
+})()
 
 
 const gameboard = (() => {
-    let gameboardArray = ['O','X','X','X','O','X','X','O','X'];
+    let gameboardArray = ['','','','','','','','',''];
    
     // return gameboardArray.forEach((el,index) => {
     //     boardSquares[index].innerHTML = el;
@@ -24,14 +32,26 @@ const gameboard = (() => {
 const displayController = (() => {
     let boardSquares = document.querySelectorAll('.boardSquare');
 
-    
     boardSquares.forEach((square) => {
         square.addEventListener('mousedown', (e) => {
-            console.log('hi', e.target.attributes.data.value);
-            console.log(gameboard.gameboardArray[e.target.attributes.data.value])
-            e.target.innerText = gameboard.gameboardArray[e.target.attributes.data.value]
+            // let {playerOne, playerTwo, lastMove, moves} = playerController;
+            if(playerController.moves<9) {
+
+                playerController.moves++
+                console.log(playerController.moves);
+                if(playerController.moves % 2 === 1) {
+                    gameboard.gameboardArray[e.target.attributes.data.value] = playerController.playerOne;
+                } else if (playerController.moves % 2 === 0) {
+                    gameboard.gameboardArray[e.target.attributes.data.value] = playerController.playerTwo;
+                }
+                // gameboard.gameboardArray[e.target.attributes.data.value] = 'hi';
+                console.log('hi', e.target.attributes.data.value);
+                console.log(gameboard.gameboardArray[e.target.attributes.data.value])
+                e.target.innerText = gameboard.gameboardArray[e.target.attributes.data.value]
+            }
         })
     })
+
 })();
 
 
