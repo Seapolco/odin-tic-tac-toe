4d96cd83a8875bfe4962a2aadcdb919ee26440f0
Seapolco <seapolco@gmail.com>
2022-09-30 22:13:57 +0100

    May have figured out a way to determine wins, worked in Run.js, about to implement.

--- app.js
+++ app.js
@@ -6,15 +6,76 @@ console.log('Hello World!');
 //factory function for players
 
 const playerController = (() => {
-    const playerOne = 'X';
-    const playerTwo = 'O';
+
+
+    const playerOne = {
+        marker: 'X',
+        placement: []
+    };
+    const playerTwo = {
+        marker: 'O',
+        placement: []
+    };
     let lastMove = '';
     let moves = 0;
+
+    let winningPositions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2],
+    ];
+
+    let winners =((playerArray, winArray) => {
+        let wins = [];
+        let winningNumbers = []
+        let streak = false;
+        
+        playerArray.forEach((el, index) => {
+          
+        
+            if(winArray.includes(el) && winningNumbers.includes(el) !== true) {
+              
+            wins.push(index);
+            winningNumbers.push(el)
+            // console.log(wins)
+            // console.log(winningNumbers)
+          }
+        
+        })
+          // console.log(wins)
+          
+          if(wins[0] + 1 === wins[1] && wins[1] + 1 === wins[2]) {
+            console.log(wins)
+            streak = true;
+          }
+          // console.log(wins)
+          // console.log(streak)
+            return streak;
+                     
+    });
+
+    let determineWin = ((winningList, winningFunction) {
+        let win = false;
+        
+        winningList.forEach((el) => {
+          if(winningFunction(ga, el) === true) {
+            win = true;
+            console.log(el)
+          }
+        })
+        return win;
+    });
+    
     let gameOver = (() => {
         console.log('Game Over!');
     })
 
-    return {playerOne, playerTwo, lastMove, moves, gameOver}
+    return {playerOne, playerTwo, lastMove, moves, winningPositions, winners, determineWin, gameOver}
 })()
 
 
@@ -46,9 +107,13 @@ const displayController = (() => {
                     playerController.moves++
                     console.log(playerController.moves);
                     if(playerController.moves % 2 === 1) {
-                        gameboard.gameboardArray[e.target.attributes.data.value] = playerController.playerOne;
+                        playerController.playerOne.placement.push(e.target.attributes.data.value);
+                        console.log(playerController.playerOne.placement);
+                        gameboard.gameboardArray[e.target.attributes.data.value] = playerController.playerOne.marker;
                     } else if (playerController.moves % 2 === 0) {
-                        gameboard.gameboardArray[e.target.attributes.data.value] = playerController.playerTwo;
+                        playerController.playerTwo.placement.push(e.target.attributes.data.value);
+                        console.log(playerController.playerTwo.placement);
+                        gameboard.gameboardArray[e.target.attributes.data.value] = playerController.playerTwo.marker;
                     }
                     // gameboard.gameboardArray[e.target.attributes.data.value] = 'hi';
                     console.log('hi', e.target.attributes.data.value);
@@ -57,6 +122,7 @@ const displayController = (() => {
                 } 
                 if (playerController.moves === 9 ){
                     playerController.gameOver();
+
                 }
 
             }
@@ -69,6 +135,8 @@ const displayController = (() => {
         });
         let newArray =  new Array(9).fill('');
         gameboard.gameboardArray = newArray;
+        playerController.playerOne.placement = [];
+        playerController.playerTwo.placement = [];
         playerController.moves = 0;
 
     })
