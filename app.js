483ecbf07db6512af983d640894b292d2f1109a1
Seapolco <seapolco@gmail.com>
2022-09-30 19:46:35 +0100

    Progress made with gameboard and displayController modules.
    Initially I supplied some basic functionality using global code, oldschool way. After I got a feel,
    I begun to refactor the code in allignment with the requirements. Definitely a struggle, this is all new to me.
    Much to be learnt.
    So far, all global code moved inside two iffy modules. gameboard array hardcoded, event listeners added so contents of array
    show in the square that was clicked (through referencing the index of game array). Did to make this dynamic.

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-console.log('Hello World!')
+console.log('Hello World!');
+
+
+// use module for gameboard and displayController
+
+//factory function for players
+
+
+
+const gameboard = (() => {
+    let gameboardArray = ['O','X','X','X','O','X','X','O','X'];
+   
+    // return gameboardArray.forEach((el,index) => {
+    //     boardSquares[index].innerHTML = el;
+    // })
+
+    return {gameboardArray}
+    
+
+
+
+})();
+
+const displayController = (() => {
+    let boardSquares = document.querySelectorAll('.boardSquare');
+
+    
+    boardSquares.forEach((square) => {
+        square.addEventListener('mousedown', (e) => {
+            console.log('hi', e.target.attributes.data.value);
+            console.log(gameboard.gameboardArray[e.target.attributes.data.value])
+            e.target.innerText = gameboard.gameboardArray[e.target.attributes.data.value]
+        })
+    })
+})();
+
+
+
+
+// boardSquares.forEach((square) => {
+//     square.addEventListener('mousedown', (e) => {
+
+//         if(lastMove === '' || lastMove === moveO) {
+//             e.target.innerText = moveX;
+//             lastMove = moveX;
+//         } else {
+//             e.target.innerText = moveO;
+//             lastMove = moveO;
+//         }
+
+//     })
+// })
+
+// let moveX = 'X';
+// let moveO = '0';
+// let lastMove = '';
